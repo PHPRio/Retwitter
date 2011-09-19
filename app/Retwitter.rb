e66a5138601cce075df607c5b3d6b3d349870dad
Igoru <igorsantos07@gmail.com>
2011-09-19 01:29:38 -0300

    finishing Retwitter

--- app/Retwitter.rb
+++ app/Retwitter.rb
@@ -1,120 +1,47 @@
 require 'rubygems'
 require 'twitter'
-require 'yaml'
 require 'ap'
 
-class Twitrends
+class Retwitter
 
-  def accounts_file= path
-    if File.file?(path) and File.readable?(path)
-      @accounts_file = path
-      @accounts = YAML::load_file path
-    else
-      raise ArgumentError, "accounts_file '#{path}' is not readable!"
-    end
+	# Initializes the object with the account data
+	# === Parameters
+	# * *account_data* +Hash+, { username => { consumer_key, consumer_secret, oauth_key, oauth_secret } }
+  def initialize account_data
+		data = account_data.to_a.first
+		@account = { :name => data[0], :data => data[1] }
   end
 
-  def initialize accounts_file_path, verbose = false
-    self.accounts_file= accounts_file_path
-    @verbose = verbose
-    @format = '[%s] %s' # [time] Trending topics
-    @start = Time.now
-  end
+	# search for search_for terms and retweets everything that's found since last check and is not from this own account, neither direct replies
+	# === Parameters
+	# * *search_for* +Array+, search terms to be retweeted
+  def retweet search_for
+		last_verified = File.open 'last_verified', 'r+'
+		last_id = last_verified.read.gsub("\000", '')
+		last_id = 0 if last_id.empty?
+		search_terms = search_for.join ' OR '
+		tweets = Twitter::Search.new.containing(search_terms).since_id(last_id).not_from(@account[:name]).fetch.collect do |t|
+			t.id unless t.text[0,1] == '@'
+		end.compact
 
-  # If the argument is false or not set, won't tweet. Only show output to know it's working properly.
-  def tweet for_real = false
-    @accounts.each_pair do |title, acc_data|
-      print "Getting Trending Topics and tweeting to #{title}..." if @verbose
-
-      @twitter = get_twitter_client acc_data
-
-      trends = get_trends acc_data['woeid']
-      if trends.length == 10
-        puts (!@verbose)? 'OK for '+title : ''
-      elsif trends.length != 0
-        puts (!@verbose)? "Something is wrong with the trends for #{title}: "+trends.inspect : ''
-      elsif trends.length == 0
-        puts "Looks like there are no trends. =( Exiting..."
-        return false
-      end
-
-      send_tweets trends, for_real
-
-      puts '' if @verbose
-    end
+		if tweets.length > 0
+			twitter = get_twitter_client
+			tweets.each { |id| twitter.retweet id }
+			last_verified.truncate 0
+			last_verified.write tweets[0]
+		end
   end
 
   private
 
-  # Configures the Twitter Client and returns a new instance of it
-  def get_twitter_client acc_data
+  # Configures the Twitter Client and returns a new instance of it, based on @account
+  def get_twitter_client
     Twitter.configure do |c|
-      c.consumer_key       = acc_data['consumer_key']
-      c.consumer_secret    = acc_data['consumer_secret']
-      c.oauth_token        = acc_data['oauth_key']
-      c.oauth_token_secret = acc_data['oauth_secret']
+      c.consumer_key       = @account[:data]['consumer_key']
+      c.consumer_secret    = @account[:data]['consumer_secret']
+      c.oauth_token        = @account[:data]['oauth_key']
+      c.oauth_token_secret = @account[:data]['oauth_secret']
     end
-
     Twitter::Client.new
   end
-
-  # Returns the trends for the location of the woeid given (Where On Earth ID).
-  # A list of places and codes can be obtained from Twitter::Client#trend_location
-  def get_trends woeid
-    trends = ''
-    got_error = false
-    while trends.empty? and (Time.now - @start) < 60 * 15 do # gives up after 15 minutes
-      begin
-        print 'Trying to connect again. ' if got_error
-        trends = @twitter.local_trends woeid
-      rescue SocketError, OpenSSL::SSL::SSLError, Errno::ECONNRESET => e
-        puts (@verbose)? ' Oops! Are you connected ('+e.class.to_s+')? Trying again in 10 seconds.' : e.class.to_s
-        got_error = true
-        sleep 10
-      end
-    end
-
-    trends
-  end
-
-  # Will tweet the trends given (preferably an Array of 10 elements) if the second argument is true;
-  # if it's false, will only pretend to tweet, to show it's working (or not)
-  def send_tweets trends, for_real
-    now  = Time.now
-    time = now.hour.to_s+'h'+('%02d'%now.min)
-		@@part = 0
-
-    [trends[5..9], trends[0..4]].each do |trends_part|
-			max_length = 50
-			begin
-				tweet = make_tweet time, trends_part, max_length
-				max_length = max_length - 2
-				puts 'giant tweet, will need to make it again...' if (!for_real && @verbose && tweet.length > 140)
-			end while tweet.length > 140
-
-      if for_real
-        if tweet.length > 140
-					@twitter.update "d igorgsantos Tweet for XX over 140 chars (#{tweet.length})! \"#{tweet[0..50]}\""
-					puts "GIANT TWEET! (#{tweet.length} chars) >> "+tweet
-				else
-	        @twitter.update tweet
-				end
-      else
-        puts "Tweet (#{tweet.length} chars) >> "+tweet if @verbose
-      end
-    end
-  end
-
-	def make_tweet time, trends, max_length
-		@@part = @@part + 1
-
-		start = (@@part == 2)? 5 : 0
-		trends_concat = trends.collect do |trend|
-			i = trends.index(trend) + start + 1
-			trend = trend[0..(max_length-4)]+'(..)' if (trend.length > max_length)
-			"#{i.to_s}. #{trend}"
-		end.join(' || ')
-		@format % [time, trends_concat]
-	end
-
 end
--- app/tt.rb
+++ app/tt.rb
@@ -1,14 +1,16 @@
-verbose = ARGV.any? { |v| v == '-v' || v == '--verbose' }
+#verbose = ARGV.any? { |v| v == '-v' || v == '--verbose' }
+#
+#if (ARGV.any? { |v| v == '-t' })
+#	for_real = true
+#	puts "[#{Time.now}] Tweeting for real. If I should't do that, CTRL+C NOW! And then, run me again without -t flag, you bastard."
+#else
+#	for_real = false
+#	puts "Entering debug mode (a.k.a. won't tweet for real). If you want to tweet, use -t flag and be happy."
+#end
 
-if (ARGV.any? { |v| v == '-t' })
-	for_real = true
-	puts "[#{Time.now}] Tweeting for real. If I should't do that, CTRL+C NOW! And then, run me again without -t flag, you bastard."
-else
-	for_real = false
-	puts "Entering debug mode (a.k.a. won't tweet for real). If you want to tweet, use -t flag and be happy."
-end
+require 'Retwitter'
+require 'yaml'
 
-require 'Twitrends'
-
-twitrends = Twitrends.new 'accounts.yaml', verbose
-twitrends.tweet for_real
+account_data = YAML::load_file 'account.yaml'
+retwitter = Retwitter.new account_data
+retwitter.retweet ['"php\'n rio"','phprio','phpnrio']
